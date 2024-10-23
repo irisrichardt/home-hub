3ecfcb5a398bc734334d1dd3fbf4b5b2d3c050d5
Íris <iris.balk04@gmail.com>
2024-10-23 18:32:38 -0300

    Estilizando navbar

--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -3,7 +3,7 @@ import { registerApplication, start, LifeCycles } from "single-spa";
 registerApplication({
   name: "@home-hub/react-navbar",
   app: () => System.import<LifeCycles>("@home-hub/react-navbar"),
-  activeWhen: (location) => location.pathname === "/",
+  activeWhen: (location) => location.pathname.includes("/"),
 });
 
 start({
