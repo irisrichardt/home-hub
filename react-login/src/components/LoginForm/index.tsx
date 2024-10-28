8f413f563d3c43c27e4ad1516e38144d4bf03312
Íris <iris.balk04@gmail.com>
2024-10-28 20:49:17 -0300

    Adicionando validação nos campos do formulário

--- react-login/src/components/LoginForm/index.tsx
+++ react-login/src/components/LoginForm/index.tsx
@@ -1,6 +1,36 @@
-import { Box, Button, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  TextField,
+  IconButton,
+  InputAdornment,
+} from "@mui/material";
+
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+  const onSubmit = (data: FormValues) => console.log(data);
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
     <Box
       sx={{
@@ -14,16 +44,46 @@ const LoginForm = () => {
         label="Email"
         variant="standard"
         sx={{ marginTop: "16px", marginX: "16px" }}
+        error={!!errors.email}
+        helperText={errors.email?.message}
+        {...register("email", {
+          required: "Campo de e-mail obrigatório.",
+          pattern: { value: /\S+@\S+\.\S+/, message: "E-mail inválido" },
+        })}
       />
       <TextField
         id="password"
-        label="Password"
+        label="Senha"
         variant="standard"
+        type={showPassword ? "text" : "password"}
         sx={{ marginTop: "16px", marginX: "16px" }}
+        error={!!errors.password}
+        helperText={errors.password?.message}
+        {...register("password", { required: "Senha obrigatória." })}
+        slotProps={{
+          input: {
+            endAdornment: (
+              <InputAdornment position="end" sx={{ mr: 2 }}>
+                <IconButton onClick={togglePasswordVisibility} edge="end">
+                  {showPassword ? (
+                    <VisibilityIcon fontSize="small" />
+                  ) : (
+                    <VisibilityOffIcon fontSize="small" />
+                  )}
+                </IconButton>
+              </InputAdornment>
+            ),
+          },
+        }}
       />
       <Button
         variant="contained"
-        sx={{ backgroundColor: "#236284", marginTop: "30px", marginX: "16px" }}
+        sx={{
+          backgroundColor: "#236284",
+          marginTop: "30px",
+          marginX: "16px",
+        }}
+        onClick={handleSubmit(onSubmit)}
       >
         Login
       </Button>
